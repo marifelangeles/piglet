5e4e73986fb684f3383e2969fe73aa6b6fe08ab5
Marifel Angeles <marifel.angeles@evereve.com>
2020-04-09 10:20:14 -0500

    run clock

--- src/components/Tracker.js
+++ src/components/Tracker.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
 import { navigate } from "@reach/router";
@@ -10,11 +10,35 @@ const Tracker = ({ type }) => {
   const currentTime = moment().format();
   const relativeTime = timeStart.from(currentTime);
 
-  console.log("relative time", relativeTime);
-
   const feedAmountTracker = useSelector(
     (state) => state.feedReducer.feedAmount
   );
+
+  // const clock = () => {
+  //   let d = new Date();
+  //   let t = d.toLocaleTimeString();
+  //   return t;
+  // };
+
+  // const timer = setInterval(clock, 1000);
+
+  // const stopClock = () => {
+  //   console.log("stop hit");
+  //   clearInterval(timer);
+  // };
+
+  // const interval = setInterval(() => {
+  //   let d = new Date();
+  //   let t = d.toLocaleTimeString();
+  //   console.log("t", t);
+  // }, 1000);
+  const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    let timerID = setInterval(() => tick(), 1000);
+    return () => clearInterval(timerID);
+  });
+  const tick = () => setDate(new Date());
 
   return (
     <div
@@ -23,6 +47,8 @@ const Tracker = ({ type }) => {
         navigate(`./${type}`);
       }}
     >
+      <h1>{date.toLocaleTimeString()}</h1>
+      {/* <button onClick={() => stopClock()}>Stop</button> */}
       <h1>Fed {relativeTime}</h1>
       <h2>{feedAmountTracker}</h2>
     </div>
