853fe4757e069198c4563a7fb5794c21a94bb4b4
Marifel Angeles <marifel.angeles@evereve.com>
2020-10-25 20:59:09 -0500

    create get and post routes

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,47 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello!");
+admin.initializeApp();
+
+const express = require("express");
+const app = express();
+
+app.get("/sleep", (req, res) => {
+  admin
+    .firestore()
+    .collection("Sleep")
+    .get()
+    .then((data) => {
+      let sleep = [];
+      data.forEach((doc) => {
+        sleep.push({
+          sleepId: doc.id,
+          start: doc.data().start,
+          end: doc.data().end,
+        });
+      });
+      return res.json(sleep);
+    })
+    .catch((err) => console.error(err));
 });
+
+app.post("/sleep", (req, res) => {
+  const newSleep = {
+    start: req.body.start,
+    end: req.body.end,
+  };
+
+  admin
+    .firestore()
+    .collection("sleep")
+    .add(newSleep)
+    .then((doc) => {
+      res.json({ message: `document ${doc.id} created successfully` });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "post /sleep error" });
+      console.error(err);
+    });
+});
+
+exports.api = functions.https.onRequest(app);
